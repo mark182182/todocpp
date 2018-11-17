c1bf41f39087ae3b79d427d6dfbb131f5bd589f3
mark182182 <versalife11@hotmail.com>
2018-11-17 10:04:51 +0100

    feat(edit): replace old task with new

--- todo.cpp
+++ todo.cpp
@@ -19,7 +19,7 @@ int main(int argc, char ** argv){
 
   keypad(menuwin, true);
 
-  std::string choices[5] = {"View tasks", "Edit a task", "Remove a task", "Exit"};
+  std::string choices[5] = {"View tasks", "Add new task", "Edit a task", "Remove a task", "Exit"};
 
   int choice;
   int highlight = 0;
@@ -49,8 +49,8 @@ int main(int argc, char ** argv){
       break;
       case KEY_DOWN:
       highlight++;
-      if(highlight == 4){
-        highlight = 3;
+      if(highlight == 5){
+        highlight = 4;
       }
       break;
       default:
@@ -67,7 +67,7 @@ int main(int argc, char ** argv){
       }
       todoFile.close();
 
-      wresize(menuwin, taskChoices.size() * 2, WIDTH + 10);
+      wresize(menuwin, HEIGHT, WIDTH + 5);
 
       int taskChoice;
       int taskHighlight = 0;
@@ -108,22 +108,80 @@ int main(int argc, char ** argv){
         }
       }
     }
-    else if (highlight == 1 && choice == 10) {
+    else if (highlight == 2 && choice == 10) {
+      std::fstream modifyTaskInFile;
+      std::string line;
+      std::vector<std::string> taskChoices;
+      modifyTaskInFile.open("todo.txt");
+      taskChoices.push_back("Back to the menu");
+      while(getline(modifyTaskInFile, line)) {
+        taskChoices.push_back(line);
+      }
+      modifyTaskInFile.close();
+      mvwprintw(menuwin, HEIGHT/3, 0, "Which task would you like to edit?");
+      wrefresh(menuwin);
+
+      int taskChoice;
+      int taskHighlight = 0;
+
+      clear();
+      wclear(menuwin);
+      refresh();
+      box(menuwin, (int)'/', (int)'-');
+      wrefresh(menuwin);
+
+      mvwprintw(menuwin, 1, 4, "Select task to edit.");
+      wrefresh(menuwin);
+
       while(1){
-        std::fstream todoFile;
-        std::string line;
-        std::vector<std::string> taskChoices;
-        todoFile.open("todo.txt");
-        while(getline(todoFile, line)) {
-          taskChoices.push_back(line);
+        for (int i = 0; i < taskChoices.size(); i++) {
+          if(i == taskHighlight){
+            wattron(menuwin, A_REVERSE);
+          }
+          mvwprintw(menuwin, (HEIGHT/5)+i+1, WIDTH/3, taskChoices[i].c_str());
+          wattroff(menuwin, A_REVERSE);
         }
-        todoFile.close();
-        mvwprintw(menuwin, HEIGHT/3, 0, "Which task would you like to edit?");
-        wrefresh(menuwin);
-        int c = getch();
+        taskChoice = wgetch(menuwin);
+
+        switch (taskChoice) {
+          case KEY_UP:
+          taskHighlight--;
+          if(taskHighlight == -1){
+            taskHighlight = 0;
+          }
+          break;
+          case KEY_DOWN:
+          taskHighlight++;
+          if(taskHighlight == taskChoices.size()) {
+            taskHighlight = taskChoices.size()-1;
+          }
+          break;
+          default:
+          break;
+        }
+        if(taskHighlight == 0 && taskChoice == 10) {
+          break;
+        }
+        else if(taskHighlight > 0 && taskChoice == 10){
+          mvprintw(2, 50, "New task name:");
+          wrefresh(menuwin);
+          move(4, 50);
+          echo();
+          char newTask[30];
+          getstr(newTask);
+          noecho();
+          taskChoices.erase(taskChoices.begin() + taskHighlight);
+          taskChoices.push_back(newTask);
+          std::ofstream writeNewTasks ("todo.txt");
+          for (int i = 1; i < taskChoices.size(); i++) {
+            writeNewTasks << taskChoices[i] << "\n";
+          }
+          modifyTaskInFile.close();
+          break;
+        }
       }
     }
-    else if (highlight == 2 && choice == 10) {
+    else if (highlight == 3 && choice == 10) {
       std::ifstream deleteFromFile ("todo.txt");
       std::string line;
       std::vector<std::string> taskChoices;
@@ -164,7 +222,7 @@ int main(int argc, char ** argv){
           case KEY_DOWN:
           taskHighlight++;
           if(taskHighlight == taskChoices.size()) {
-            taskHighlight = taskChoices.size()-1;
+            taskHighlight = taskChoices.size() - 1;
           }
           break;
           default:
@@ -184,7 +242,7 @@ int main(int argc, char ** argv){
         }
       }
     }
-    else if (highlight == 3 && choice == 10) {
+    else if (highlight == 4 && choice == 10) {
       break;
     }
   }
