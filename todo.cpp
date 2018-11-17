e4f1908d45dfc60b8fa3146c4eee32e5db856409
mark182182 <versalife11@hotmail.com>
2018-11-17 09:08:33 +0100

    feat(remove): now working with 1^99 if statements and while loops

--- todo.cpp
+++ todo.cpp
@@ -124,64 +124,60 @@ int main(int argc, char ** argv){
       }
     }
     else if (highlight == 2 && choice == 10) {
+      std::ifstream deleteFromFile ("todo.txt");
+      std::string line;
+      std::vector<std::string> taskChoices;
+      taskChoices.push_back("Back to the menu");
+      while(getline(deleteFromFile, line)) {
+        taskChoices.push_back(line);
+      }
+
+      int taskChoice;
+      int taskHighlight = 0;
+
+      clear();
+      wclear(menuwin);
+      refresh();
+      box(menuwin, (int)'/', (int)'-');
+      wrefresh(menuwin);
+
+      mvwprintw(menuwin, 1, 4, "Select task to remove.");
+      wrefresh(menuwin);
+
       while(1) {
-        std::ifstream deleteFromFile ("todo.txt");
-        std::string line;
-        std::vector<std::string> taskChoices;
-        taskChoices.push_back("Back to the menu");
-        while(getline(deleteFromFile, line)) {
-          taskChoices.push_back(line);
+        for (int i = 0; i < taskChoices.size(); i++) {
+          if(i == taskHighlight){
+            wattron(menuwin, A_REVERSE);
+          }
+          mvwprintw(menuwin, (HEIGHT/5)+i+1, WIDTH/3, taskChoices[i].c_str());
+          wattroff(menuwin, A_REVERSE);
         }
+        taskChoice = wgetch(menuwin);
 
-        int taskChoice;
-        int taskHighlight = 0;
-
-        clear();
-        wclear(menuwin);
-        refresh();
-        box(menuwin, (int)'/', (int)'-');
-        wrefresh(menuwin);
-
-        mvwprintw(menuwin, 1, 4, "Select task to remove.");
-        wrefresh(menuwin);
-
-        while(1) {
-          for (int i = 0; i < taskChoices.size(); i++) {
-            if(i == taskHighlight){
-              wattron(menuwin, A_REVERSE);
-            }
-            mvwprintw(menuwin, (HEIGHT/5)+i+1, WIDTH/3, taskChoices[i].c_str());
-            wattroff(menuwin, A_REVERSE);
+        switch (taskChoice) {
+          case KEY_UP:
+          taskHighlight--;
+          if(taskHighlight == -1){
+            taskHighlight = 0;
           }
-          taskChoice = wgetch(menuwin);
-
-          switch (taskChoice) {
-            case KEY_UP:
-            taskHighlight--;
-            if(taskHighlight == -1){
-              taskHighlight = 0;
-            }
-            break;
-            case KEY_DOWN:
-            taskHighlight++;
-            if(taskHighlight == taskChoices.size()) {
-              taskHighlight = taskChoices.size()-1;
-            }
-            break;
-            default:
-            break;
+          break;
+          case KEY_DOWN:
+          taskHighlight++;
+          if(taskHighlight == taskChoices.size()) {
+            taskHighlight = taskChoices.size()-1;
           }
-          if(taskHighlight == 0 && taskChoice == 10) {
-            break;
-          }
+          break;
+          default:
+          break;
         }
-        taskChoices.erase(taskChoices.begin() + taskHighlight);
-        if(deleteFromFile.is_open()) {
-          std::ofstream writeNewTasks ("whatever.txt");
-          for (int i = 0; i < taskChoices.size(); i++) {
+        if(taskHighlight == 0 && taskChoice == 10) {
+          break;
+        }
+        else if(taskHighlight > 0 && taskChoice == 10){
+          taskChoices.erase(taskChoices.begin() + taskHighlight);
+          std::ofstream writeNewTasks ("todo.txt");
+          for (int i = 1; i < taskChoices.size(); i++) {
             writeNewTasks << taskChoices[i] << "\n";
-            mvwprintw(menuwin, HEIGHT/2+i, i, taskChoices[i].c_str());
-            wrefresh(menuwin);
           }
           deleteFromFile.close();
           break;
