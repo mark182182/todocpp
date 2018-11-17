d19d5cecfb904202927868a67184fbc39940e6a5
mark182182 <versalife11@hotmail.com>
2018-11-17 10:28:17 +0100

    feat(add): add task option

--- todo.cpp
+++ todo.cpp
@@ -108,6 +108,67 @@ int main(int argc, char ** argv){
         }
       }
     }
+    else if (highlight == 1 && choice == 10) {
+      std::vector<std::string> menuChoices;
+      menuChoices.push_back("Back to the menu");
+      menuChoices.push_back("Add new task");
+
+      int taskChoice;
+      int taskHighlight = 0;
+
+      clear();
+      wclear(menuwin);
+      refresh();
+      box(menuwin, (int)'/', (int)'-');
+      wrefresh(menuwin);
+
+      while(1){
+        for (int i = 0; i < menuChoices.size(); i++) {
+          if(i == taskHighlight){
+            wattron(menuwin, A_REVERSE);
+          }
+          mvwprintw(menuwin, (HEIGHT/5)+i+1, WIDTH/3, menuChoices[i].c_str());
+          wattroff(menuwin, A_REVERSE);
+        }
+        taskChoice = wgetch(menuwin);
+
+        switch (taskChoice) {
+          case KEY_UP:
+          taskHighlight--;
+          if(taskHighlight == -1){
+            taskHighlight = 0;
+          }
+          break;
+          case KEY_DOWN:
+          taskHighlight++;
+          if(taskHighlight == menuChoices.size()) {
+            taskHighlight = menuChoices.size() - 1;
+          }
+          break;
+          default:
+          break;
+        }
+        if(taskHighlight == 0 && taskChoice == 10) {
+          break;
+        }
+        else if(taskHighlight > 0 && taskChoice == 10){
+          mvprintw(2, 50, "Please specify the task name:");
+          wrefresh(menuwin);
+          move(4, 50);
+          echo();
+          char newTask[30];
+          getstr(newTask);
+          noecho();
+          menuChoices.push_back(newTask);
+          std::ofstream writeNewTasks ("todo.txt");
+          for (int i = 2; i < menuChoices.size(); i++) {
+            writeNewTasks << menuChoices[i] << "\n";
+          }
+          writeNewTasks.close();
+          break;
+        }
+      }
+    }
     else if (highlight == 2 && choice == 10) {
       std::fstream modifyTaskInFile;
       std::string line;
